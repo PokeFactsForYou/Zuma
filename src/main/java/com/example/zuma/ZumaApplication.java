36fd70ef49dafac1ba1460be3766769e1a7718ae
Justin <hohman.justin.m@gmail.com>
2018-03-30 22:55:58 -0400

    Add Spring Initializr - Using Gradle

---
+++ src/main/java/com/example/zuma/ZumaApplication.java
@@ -0,0 +1,12 @@
+package com.example.zuma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZumaApplication.class, args);
+	}
+}
