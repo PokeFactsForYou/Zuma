9ef9a897d6fb3072e63958b11b31184a7ae5bb48
PokeFactsForYou <hohman.justin.m@gmail.com>
2018-04-13 21:25:53 -0400

    Add Webpack Configuration for both prod and dev

---
+++ frontend/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => <div> Works! </div>
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ webpack.production.config.js
@@ -0,0 +1,28 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./frontend/index.html",
+    filename: "./index.html"
+});
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins: [htmlPlugin]
+};
